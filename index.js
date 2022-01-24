6d44740bc6418b30374550177cbdca5382fe2a45
unknown <federico.stagnaro.ar@gmail.com>
2022-01-24 01:06:20 -0300

    build frontend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 
 app.use(express.json())  // Parser de json-data
 
-//app.use(express.static("build"))
+app.use(express.static("build"))
 
 morgan.token("body", (req,res) => {
     return JSON.stringify(req.body)
